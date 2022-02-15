4a44814aca12302783dd983907c020a2bb513624
@malijonov2000 <@malijonov2000@gmail.com>
2022-02-15 16:43:51 +0500

    Loading table ga qo'shildi

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,2 +1,2 @@
 // export const BaseUrl = "http://farruhrg.beget.tech/api"
-export const BaseUrl = "https://3862-213-230-100-230.ngrok.io/api"
+export const BaseUrl = "https://527Loac-213-230-100-230.ngrok.io/api"
