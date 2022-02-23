13cb984e016920868b04b7dbf7a321cf2c909610
@malijonov2000 <@malijonov2000@gmail.com>
2022-02-23 20:27:23 +0500

    nothing

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,1 +1,4 @@
-export const BaseUrl = "https://830f-84-54-94-234.ngrok.io/api";
+// export const BaseUrl = "http://farruhrg.beget.tech/api"
+// export const BaseUrl = "https://aqlli-uy.uz/api"
+export const Base = "https://830f-84-54-94-234.ngrok.io";
+export const BaseUrl = Base+"/api";
