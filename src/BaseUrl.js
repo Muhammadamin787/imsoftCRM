d954295c29a4c5699937dff0285af11281366fbe
@malijonov2000 <@malijonov2000@gmail.com>
2022-02-24 18:07:19 +0500

    Pane da hamma narsani saqlaydi

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,2 +1,3 @@
-export const Base = "https://a219-92-63-204-6.ngrok.io";
+// export const Base = "https://a219-92-63-204-6.ngrok.io";
+export const Base = "https://aqlli-uy.uz";
 export const BaseUrl = Base + "/api";
