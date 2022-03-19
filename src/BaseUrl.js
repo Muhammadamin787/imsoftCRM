db75faa5df27a65661088752872f0db226a2b4ce
Hojiakbar <hojiakbaranorboyev2101@gmail.com>
2022-03-19 13:05:08 +0500

    asda

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,3 +1,3 @@
 // export const Base = "https://aqlli-uy.uz/";
-export const Base = "https://server.ddparfum.uz/";
+export const Base = "https://imsoftgroup.uz/";
 export const BaseUrl = Base + "api/v1";
